43f0e4fd25eb5ace62643d83a7d695c01a49a81f
HunterPsycho100 <67747295+HunterPsycho100@users.noreply.github.com>
2021-04-05 14:30:14 -0700

    code ready

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const http = require('http');
 
-cont port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 const server = http.createServer((request, response) => {
 	response.statusCode = 200;
 	response.setHeader =('Constent-Type', 'text/plain');
-	respond.end(Hello World\n\nCool!');
+	response.end('Hello World\n\nCool!');
 });
 
 server.listen(port, (err) => {
 	if(err) {
 		return console.log('Something went wrong ', err);
 	}
-	console.log(`Server Listening on: ${port}`);
+	console.log('Server Listening on: ${port}');
 });
